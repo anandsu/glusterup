cf803d2627ae2050f489335c4f47ba767d05f013
Anand Subramanian <anands@redhat.com>
2014-09-13 04:09:02 +0530

    Correct set of files for initial drop of upcalls xlator with some unnecessary object files removed
    Signed-off-by: Anand Subramanian <anands@redhat.com>

---
+++ xlators/features/upcalls/src/upcalls.c
@@ -0,0 +1,69 @@
+/*
+   Copyright (c) 2006-2014 Red Hat, Inc. <http://www.redhat.com>
+   This file is part of GlusterFS.
+
+   This file is licensed to you under your choice of the GNU Lesser
+   General Public License, version 3 or any later version (LGPLv3 or
+   later), or the GNU General Public License, version 2 (GPLv2), in all
+   cases as published by the Free Software Foundation.
+*/
+#include <unistd.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <pthread.h>
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+#include "config.h"
+#endif
+
+#include "glusterfs.h"
+#include "compat.h"
+#include "xlator.h"
+#include "inode.h"
+#include "logging.h"
+#include "common-utils.h"
+
+#include "statedump.h"
+#include "syncop.h"
+
+#ifndef LLONG_MAX
+#define LLONG_MAX LONG_LONG_MAX /* compat with old gcc */
+#endif /* LLONG_MAX */
+
+
+int
+up_open_cbk (call_frame_t *frame, void *cookie, xlator_t *this,
+             int32_t op_ret, int32_t op_errno, fd_t *fd, dict_t *xdata)
+{
+
+        if (op_ret < 0)
+                goto unwind;
+
+unwind:
+        STACK_UNWIND_STRICT (open, frame, op_ret, op_errno, fd, xdata);
+
+        return 0;
+}
+
+
+int
+up_open (call_frame_t *frame, xlator_t *this, loc_t *loc, int32_t flags,
+         fd_t *fd, dict_t *xdata)
+{
+        STACK_WIND (frame, up_open_cbk,
+                    FIRST_CHILD(this), FIRST_CHILD(this)->fops->open,
+                    loc, flags, fd, xdata);
+
+        return 0;
+}
+
+struct xlator_fops fops = {
+        .open        = up_open,
+};
+
+struct volume_options options[] = {
+        { .key = {NULL} },
+};
+
+
