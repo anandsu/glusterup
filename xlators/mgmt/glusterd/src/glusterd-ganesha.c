c069912d98379bc43ce32a8c2c598a73d9572fce
Meghana Madhusudhan <mmadhusu@redhat.com>
2014-12-04 19:45:33 +0530

    Set nfs.disable on all the volumes in the trusted pool. Signed-off-by: Meghana Madhusudhan <mmadhusu@redhat.com>

--- xlators/mgmt/glusterd/src/glusterd-ganesha.c
+++ xlators/mgmt/glusterd/src/glusterd-ganesha.c
@@ -38,7 +38,7 @@ return 1;
 
 int  ganesha_add_export(char *volname)
 {
-runner_t runner = {0,1};
+runner_t runner = {0,};
 int ret = -1;
 runinit (&runner);
 create_export_config(volname);
@@ -48,23 +48,42 @@ ret = runner_run_nowait(&runner);
 return ret;
 }
 
-
-
-int stop_ganesha(dict_t *dict,char **op_errstr)
+int tear_down_cluster(dict_t *dict)
 {
-runner_t                runner                     = {0,};
 int ret = -1;
-
-runinit (&runner);
-runner_add_args (&runner, "pkill", "ganesha.nfsd",NULL);
-ret = runner_run_reuse (&runner);
+runner_t runner = {0,};
 if (is_origin_glusterd(dict))
 {
 gf_log ( "",GF_LOG_INFO,"before teardown");
 runinit (&runner);
 runner_add_args (&runner, "sh","/etc/ganesha/ganesha-ha.sh","teardown",NULL);
 }
+ret = runner_run_nowait(&runner);
+return ret;
+}
 
+int setup_cluster(dict_t *dict)
+{
+int ret = -1;
+runner_t runner = {0,};
+if (is_origin_glusterd(dict))
+{
+        gf_log ("",GF_LOG_INFO, "I am originator glusterd");
+       runinit (&runner);
+       runner_add_args (&runner, "sh","/etc/ganesha/ganesha-ha.sh","setup",NULL);
+
+}
+ret =  runner_run_nowait(&runner);
+return ret;
+
+}
+int stop_ganesha(dict_t *dict,char **op_errstr)
+{
+runner_t                runner                     = {0,};
+int ret = -1;
+tear_down_cluster(dict);
+runinit (&runner);
+runner_add_args (&runner, "pkill", "ganesha.nfsd",NULL);
 ret = runner_run_nowait(&runner);
 return ret;
 }
@@ -74,21 +93,76 @@ int start_ganesha(dict_t *dict, glusterd_volinfo_t *volinfo)
 
 runner_t                runner                     = {0,};
 int ret = -1;
+char key[1024] = {0,};
+long int i =1;
 dict_t *vol_opts =  NULL;
-vol_opts = volinfo->dict;
+glusterd_volinfo_t *volinfo1 = NULL;
+int count =0;
+dict_t *dict1 = NULL;
+char *volname =  NULL;
+glusterd_conf_t *priv = NULL;
 
-ret = dict_set_str(vol_opts, "nfs.disable","on");
-runinit (&runner);
+priv =  THIS->private;
+GF_ASSERT(priv);
+
+dict1 = dict_new();
+if (!dict)
+        goto out;
+
+//vol_opts = volinfo->dict;
+
+/*ret = dict_get_int32 (dict, key, &volcount);
+gf_log ("", GF_LOG_INFO, "number of volumes is %d", volcount);
+        if (ret) {
+                gf_log ("", GF_LOG_INFO, "failed to get volcount");
+                goto out;
+        }
+        if (volcount <= 0) {
+                ret = -1;
+                goto out;
+        }
+
+for (i = 1; i <= volcount; i++) {
+                snprintf (key, sizeof (key), "volname%ld", i);
+                ret = dict_get_str (dict, key, &volname);
+                if (ret) {
+                        gf_log ("", GF_LOG_ERROR, "failed to get the "
+                                "volname");
+                        goto out;
+                }
+
+                ret = glusterd_volinfo_find (volname, &volinfo1);
+                if (ret) {
+                        gf_log ("", GF_LOG_ERROR, "volinfo for %s "
+                                "not found", volname);
+                        goto out;
+                }
+                vol_opts = volinfo1->dict;
+                ret = dict_set_str(vol_opts, "nfs.disable","on");
+}
+*/
+//runinit (&runner);
+        list_for_each_entry(volinfo1,&priv->volumes, vol_list) {
+                memset (key, 0, sizeof (key));
+                snprintf (key, sizeof (key), "volume%d", count);
+                ret = dict_set_str (dict1, key, volinfo1->volname);
+                if (ret)
+                        goto out;
+                vol_opts = volinfo1->dict;
+                ret = dict_set_str(vol_opts, "nfs.disable","on");
+
+                count++;
+
+        }
+
 glusterd_nfs_server_stop();
-
+runinit(&runner);
 runner_add_args (&runner, "/usr/bin/ganesha.nfsd",
                          "-L", "/nfs-ganesha-op.log",
                          "-f","/etc/ganesha/nfs-ganesha.conf","-N", "NIV_FULL_DEBUG",NULL);
-ret = runner_run_nowait(&runner);
-if (is_origin_glusterd(dict))
-        gf_log ("",GF_LOG_INFO, "I am originator glusterd");
-
-return ret;
+ret =  runner_run_nowait(&runner);
+ret = setup_cluster(dict);
+out : return ret;
 }
 
 
